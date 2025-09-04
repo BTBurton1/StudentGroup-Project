79cd66294581ab33afdc7f0e080c575a114fc38d
BTBurton1 <bilal.burton123@gmail.com>
2025-09-04 14:36:40 -0400

    Added the UML Design for the functions and its datatypes

---
+++ StudentGroup-Project/StudentGroup.cs
@@ -0,0 +1,24 @@
+
+public class StudentGroup
+{
+    private string GroupName;
+    private integer NumberOfMembers;
+    private string[] NumofStudents;
+
+    public StudentGroup()
+    {
+
+    }
+    public AddMember()
+    {
+
+    }
+    public RetrieveMember()
+    {
+
+    }
+    public void DisplayGroup()
+    {
+        
+    }
+}

4d701d22fbcc389e23781e6c8ad770396fc36491
Webbney Vallon <webbvallon1999@gmail.com>
2025-09-07 21:45:47 -0400

    Developed constructor and display method

--- StudentGroup-Project/StudentGroup.cs
+++ StudentGroup-Project/StudentGroup.cs
@@ -1,24 +1,42 @@
 
+using System.Formats.Tar;
+
+public class Program
+{
+    public static void Main()
+    {
+        
+        StudentGroup group = new StudentGroup("CSC 350H", 4);
+        
+        Console.WriteLine($"Group Name: {group.getGRoupName()}");
+        group.DisplayGroup();
+    }
+}
+
 public class StudentGroup
 {
     private string GroupName;
-    private integer NumberOfMembers;
+    private int NumberOfMembers;
     private string[] NumofStudents;
 
-    public StudentGroup()
+    public StudentGroup(string groupName, int numOfMembers)
     {
-
+        GroupName = groupName;
+        NumberOfMembers = numOfMembers;
     }
-    public AddMember()
-    {
+    // public AddMember(int numOfMembers)
+    // {
+    //     NumofStudents = new string [numOfMembers];
+    // }
+    // public RetrieveMember()
+    // {
 
-    }
-    public RetrieveMember()
-    {
+    // }
+    public string getGRoupName()
+    { return GroupName; }
 
-    }
     public void DisplayGroup()
     {
-        
+        Console.WriteLine($" Group: {GroupName}");
     }
 }
